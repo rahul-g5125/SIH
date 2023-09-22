6bbaab26a4d7d6a67c8387e03094323a61a48fdf
rahul-g5125 <rahul.h.gandla@gmail.com>
2023-09-22 21:01:08 +0530

    Navbar added

--- smart-india-hackathon/src/App.jsx
+++ smart-india-hackathon/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-function App(){
-    return <h1>Hello World!</h1>
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- smart-india-hackathon/src/components/Navbar.jsx
+++ smart-india-hackathon/src/components/Navbar.jsx
@@ -1,4 +1,40 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+
+import { Drawer, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const PageName = "Project Portal";
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav>
+      <Toolbar>
+        <IconButton onClick={toggleMenu} color="inherit">
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, textAlign: "center" }}
+        >
+          {PageName}
+        </Typography>
+      </Toolbar>
+      <Drawer anchor="left" open={isOpen} onClose={toggleMenu}>
+        <div role="presentation" onClick={toggleMenu} onKeyDown={toggleMenu}>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+          </ul>
+        </div>
+      </Drawer>
+    </nav>
+  );
 };
 export default Navbar;
