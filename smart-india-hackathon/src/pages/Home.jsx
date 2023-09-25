e70ecbd392a15752cf6373df8937ca8ebae28807
Leslie074 <leslieprojects74@gmail.com>
2023-09-25 19:52:33 +0530

    Added ProjectBox Component

--- smart-india-hackathon/src/pages/Home.jsx
+++ smart-india-hackathon/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import ProjectBox from "../components/ProjectBox";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <ProjectBox />
     </div>
   );
 }
